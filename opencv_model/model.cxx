7fa6f530e6ced864bb243a0a3f72fff9e2016899
Jesse Rosalia <jesse.rosalia@gatech.edu>
2013-10-07 23:46:34 -0400

    added magic courtesy of nick and josh to project camera frame onto 2d image of road (top down) for model fitting

--- opencv_model/model.cxx
+++ opencv_model/model.cxx
@@ -10,6 +10,7 @@ void cvShowImageMat(const char *name, Mat& mat);
 void getBinary(Mat& src, Scalar& low_HSV, Scalar& hi_HSV, Mat& dest);
 void cannyThreshold(Mat& src, Mat& srcGray, Mat& dst, Mat& dstMask);
 void removeSmall(Mat& src, Mat& dest, double minRadius);
+void groundTransform(Mat& src, Mat& ground);
 
 Scalar LOW_HSV_EDGE = Scalar(20, 0, 0);
 Scalar HIGH_HSV_EDGE = Scalar(40, 255, 255);
@@ -123,8 +124,8 @@ int main ( int argc, char **argv )
 	if(!cap.isOpened())  // check if we succeeded
         return -1;
 	
-    // cvNamedWindow( "ImageA", 0 );
 	cvNamedWindow( "ImageB", 0 );
+    cvNamedWindow( "TransformB", 0 );
     // cvNamedWindow( "LKpyr_OpticalFlow", 0 );
 
 	int flipMode = -1;
@@ -146,10 +147,15 @@ int main ( int argc, char **argv )
         maskA = maskA + thold + edgeMask;
         removeSmall(maskA, maskA, 100);
         
+        Mat gt;
+        groundTransform(imgA, gt);
+        
 		pullFrame(cap, imgB, imgGrayB, NULL); //flipHorizAndVert);
 
         // cvShowImageMat( "ImageA", thold );
-        cvShowImageMat( "ImageB", maskA );
+        cvShowImageMat( "ImageB", imgA );
+        cvShowImageMat( "TransformB", gt);
+        
         // cvShowImageMat( "LKpyr_OpticalFlow", imgGrayA );
 		imgA = imgB;
         imgGrayA = imgGrayB;
@@ -158,6 +164,96 @@ int main ( int argc, char **argv )
 	}
 }
 
+// void lensTransform(Point& pt, double h, double cameraAngle, double& vec[2]) {
+// }
+// void groundTransform(Point& framePt, Point& groundPt, double h, double cameraAngle) {
+//     double vec[2];
+//     lensTransform(framePt, h, cameraAngle, vec);
+//     double x = sqrt( (pow(-h / cos(vec[0]), 2) - pow(h, 2))
+//                       / (1 + pow(tan(vec[1]), 2)) );
+//     double y = x * tan(phi);
+//     
+// }
+
+// void groundTransform(Mat& src, Mat& ground) {
+//     int width = 1280;
+//     int height = 720;
+//     
+//     int x_res = 400;
+//     int y_res = 400;
+// 
+//     ground = Mat::zeros(cvSize(x_res, y_res), src.type());
+// 
+//     double groundWidth = 20.0; //ft
+//     double groundHeight = 20.0; //ft
+//     double h = 21.0/12.0; //ft
+//     double deg_fov = 120.0;
+//     double phi_scale = 100000 * (M_PI / 180) * (deg_fov / width);
+//     double theta_scale = 100000 * (M_PI / 180) * (deg_fov / width);
+//     int x_frame_off = width/2;
+//     int y_frame_off = 0;
+//     
+//     for (int x = 0; x < x_res; x++) {
+//         for (int y = 0; y < y_res; y++) {
+//             double x_ground = x / groundWidth - (groundWidth/2);
+//             double y_ground = y / groundHeight;
+//             
+//             double theta = acos(-h/ sqrt(pow(x_ground, 2) + pow(y_ground, 2) + pow(h, 2)));
+//             double phi = atan2(y_ground, x_ground);
+//             int x_frame = phi * phi_scale + x_frame_off;
+//             int y_frame = theta * theta_scale + y_frame_off;
+//             ground.at<double>(y, x) = src.at<double>(y_frame, x_frame);
+//             if (x == 0 && y == 0) {
+//                 printf("%d, %d, %f, %f, %f, %f, %f\n", x_frame, y_frame, src.at<double>(y_frame, x_frame), theta, phi, theta_scale, phi_scale);
+//             }
+//         }
+//     }   
+// }
+
+void groundTransform(Mat& src, Mat& ground) {
+    int width = 1280;
+    int height = 720;
+    
+    int x_res = 800;
+    int y_res = 800;
+
+    ground = Mat::zeros(cvSize(x_res, y_res), src.type());
+
+    double groundWidth = 30.0; //ft
+    double groundHeight = 30.0; //ft
+    double h = 21.0/12.0; //ft
+    double deg_fov = (120.0 / 180.0) * M_PI;
+    double phi_scale = width / deg_fov;
+    double theta_scale = height / deg_fov * 1.0;
+    double x_frame_off = x_res/2;
+    double y_frame_off = y_res/10;
+    double phi_off = -1.15;
+    double theta_off = -1.55;
+    
+    for (int x = 0; x < x_res; x++) {
+        for (int y = 0; y < y_res; y++) {
+            double x_ground = x * groundWidth / x_res - (groundWidth / 2);
+            double y_ground = y * groundHeight / y_res;
+            
+            double theta = acos(-h/ sqrt(pow(x_ground, 2) + pow(y_ground, 2) + pow(h, 2)));
+            double phi = atan2(y_ground, x_ground);
+            int x_frame = (phi + phi_off) * phi_scale + x_frame_off;
+            int y_frame = (theta + theta_off) * theta_scale + y_frame_off;
+            // if (x == 0 && y == 0) {
+            //     printf("%d, %d, %f, %f, %f, %f\n", x_frame, y_frame, theta, phi, theta_scale, phi_scale);
+            // }
+            if (y_frame < 0 || y_frame >= height || x_frame < 0 || x_frame >= width) {
+                // printf("%d, %d\n", x, y);
+//                ground.at<double>(y, x) = 0;
+            } else {
+                // printf("%d, %d, %f\n", x, y,  src.at<Vec3b>(y_frame, x_frame));
+                
+                ground.at<Vec3b>(y_res-y, x_res-x) = src.at<Vec3b>(y_frame, x_frame);
+            }
+        }
+    }
+    
+}
 void cvShowImageMat(const char *name, Mat& mat) {
 	IplImage img = mat;
 	cvShowImage(name, &img);
